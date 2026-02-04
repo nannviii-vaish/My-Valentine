77b4f8e5a1ae50faa5331e1969b64b8b6f09c173
nannviii-vaish <nannviii@gmail.com>
2026-02-04 19:54:56 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const text = document.getElementById("text");
-const yes = document.getElementById("yes");
-const no = document.getElementById("no");
+// const text = document.getElementById("text");
+// const yes = document.getElementById("yes");
+// const no = document.getElementById("no");
 
-text.innerHTML = "Koraga, will you be my Valentine?";
+// text.innerHTML = "Koraga, will you be my Valentine?";
 
-// No button runs away
-no.addEventListener("mouseover", () => {
-    no.style.position = "absolute";
-    no.style.left = Math.random() * 80 + "%";
-    no.style.top = Math.random() * 80 + "%";
-});
+// // No button runs away
+// no.addEventListener("mouseover", () => {
+//     no.style.position = "absolute";
+//     no.style.left = Math.random() * 80 + "%";
+//     no.style.top = Math.random() * 80 + "%";
+// });
 
-// Yes redirects
-yes.addEventListener("click", () => {
-    window.location.href = "yes.html";
-});
+// // Yes redirects
+// yes.addEventListener("click", () => {
+//     window.location.href = "yes.html";
+// });
